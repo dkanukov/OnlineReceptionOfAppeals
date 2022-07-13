b86078a8a36d57e2efa4498124114232920803fe
Denis <kanukov.denis@gmail.com>
2022-07-13 20:31:31 +0300

    adding form validation

--- mysite/home/static/js/donateForm.js
+++ mysite/home/static/js/donateForm.js
@@ -1,17 +1,11 @@
+const btnSubmit = document.getElementById('submitForm');
+const nameInput = document.getElementById('nameFormInput');
+const phoneInput = document.getElementById('phoneFormInput');
+const emailInput = document.getElementById('mailFormInput');
+const messageInput = document.getElementById('messageFormInput');
+const toggler = document.getElementById('toggler');
+let canSubmit = false;
 (function () {
-    const formBtn = document.getElementById('formBtn');
-    formBtn.addEventListener('click', function (e) {
-        e.preventDefault();
-        console.log('clicked');
-    });
+
 })();
 
-// async function fetchData() {
-//     const res = fetch('/api/donate', {
-//         method: 'GET',
-//         headers: {
-//             'Content-Type': 'application/json',
-//             'ID': '12'
-//         }
-//     });
-// }
