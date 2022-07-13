98b87f98a4d54b8603a20e89d7345968e26c3dd6
Denis <kanukov.denis@gmail.com>
2022-07-13 21:03:30 +0300

    some more validation of form

--- mysite/home/static/js/donateForm.js
+++ mysite/home/static/js/donateForm.js
@@ -3,9 +3,31 @@ const nameInput = document.getElementById('nameFormInput');
 const phoneInput = document.getElementById('phoneFormInput');
 const emailInput = document.getElementById('mailFormInput');
 const messageInput = document.getElementById('messageFormInput');
-const toggler = document.getElementById('toggler');
-let canSubmit = false;
-(function () {
+const checkBox = document.getElementById('checkboxForm');
 
+function isValid() {
+    // TODO: need to add extra check of phone and email
+    //      add mask for phone and email
+    //      send form if valid
+    if (nameInput.value.length > 0 && phoneInput.value.length > 0 && emailInput.value.length > 0 && checkBox.checked) {
+        btnSubmit.disabled = false;
+    } else {
+        btnSubmit.disabled = true;
+    }
+}
+
+(function () {
+    nameInput.addEventListener('input', function () {
+        isValid();
+    });
+    phoneInput.addEventListener('input', function (e) {
+        isValid();
+    });
+    emailInput.addEventListener('input', function () {
+        isValid();
+    });
+    checkBox.addEventListener('change', function () {
+        isValid();
+    });
 })();
 
