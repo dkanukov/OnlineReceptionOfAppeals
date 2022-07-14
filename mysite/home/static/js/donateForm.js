ee0439e30fbdb1048c16a68425cc84a8efb548e6
Denis <kanukov.denis@gmail.com>
2022-07-14 21:09:54 +0300

    finish with modal and data validation

--- mysite/home/static/js/donateForm.js
+++ mysite/home/static/js/donateForm.js
@@ -5,11 +5,15 @@ const emailInput = document.getElementById('mailFormInput');
 const messageInput = document.getElementById('messageFormInput');
 const checkBox = document.getElementById('checkboxForm');
 
+document.addEventListener('DOMContentLoaded', () => {
+    IMask(phoneInput, {mask: '+{7}(000)000-00-00'});
+});
+
 function isValid() {
-    // TODO: need to add extra check of phone and email
-    //      add mask for phone and email
-    //      send form if valid
-    if (nameInput.value.length > 0 && phoneInput.value.length > 0 && emailInput.value.length > 0 && checkBox.checked) {
+    console.log(phoneInput.value.length);
+    if (nameInput.value.length > 0 && (phoneInput.value.length > 0 && phoneInput.value.length === 16)
+        && (emailInput.value.length > 0 && emailInput.value.toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/))
+        && checkBox.checked) {
         btnSubmit.disabled = false;
     } else {
         btnSubmit.disabled = true;
@@ -28,6 +32,13 @@ function isValid() {
     });
     checkBox.addEventListener('change', function () {
         isValid();
+    });
+    btnSubmit.addEventListener('click', function () {
+        //    here will be form sending dont forget about trim() for inputs
+        nameInput.value = '';
+        phoneInput.value = '';
+        emailInput.value = '';
+        messageInput.value = '';
     });
 })();
 
