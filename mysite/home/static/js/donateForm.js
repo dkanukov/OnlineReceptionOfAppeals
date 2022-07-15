7342a16a84a6ebb7b88a929faf8b8ad54dd5a98f
Denis <kanukov.denis@gmail.com>
2022-07-15 12:33:36 +0300

    refactor modal and script for smaller devices

--- mysite/home/static/js/donateForm.js
+++ mysite/home/static/js/donateForm.js
@@ -7,16 +7,26 @@ const checkBox = document.getElementById('checkboxForm');
 
 document.addEventListener('DOMContentLoaded', () => {
     IMask(phoneInput, {mask: '+{7}(000)000-00-00'});
+    IMask(document.getElementById('phoneFormInputXs'), {mask: '+{7}(000)000-00-00'});
 });
 
 function isValid() {
-    console.log(phoneInput.value.length);
     if (nameInput.value.length > 0 && (phoneInput.value.length > 0 && phoneInput.value.length === 16)
         && (emailInput.value.length > 0 && emailInput.value.toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/))
         && checkBox.checked) {
         btnSubmit.disabled = false;
     } else {
         btnSubmit.disabled = true;
+    }
+}
+
+function isValidXs() {
+    if (document.getElementById('nameFormInputXs').value.length > 0 && (document.getElementById('phoneFormInputXs').value.length > 0 && document.getElementById('phoneFormInputXs').value.length === 16)
+        && (document.getElementById('mailFormInputXs').value.length > 0 && document.getElementById('mailFormInputXs').value.toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/))
+        && document.getElementById('checkboxFormXs').checked) {
+        document.getElementById('submitFormXs').disabled = false;
+    } else {
+        document.getElementById('submitFormXs').disabled = true;
     }
 }
 
@@ -39,6 +49,27 @@ function isValid() {
         phoneInput.value = '';
         emailInput.value = '';
         messageInput.value = '';
+    });
+
+    // validating form for smaller devices
+    document.getElementById('nameFormInputXs').addEventListener('input', function () {
+        isValidXs();
+    });
+    document.getElementById('phoneFormInputXs').addEventListener('input', function (e) {
+        isValidXs();
+    });
+    document.getElementById('mailFormInputXs').addEventListener('input', function () {
+        isValidXs();
+    });
+    document.getElementById('checkboxFormXs').addEventListener('change', function () {
+        isValidXs();
+    });
+    document.getElementById('submitFormXs').addEventListener('click', function () {
+        //    here will be form sending dont forget about trim() for inputs
+        document.getElementById('nameFormInputXs').value = '';
+        document.getElementById('phoneFormInputXs').value = '';
+        document.getElementById('mailFormInputXs').value = '';
+        document.getElementById('messageFormInputXs').value = '';
     });
 })();
 
