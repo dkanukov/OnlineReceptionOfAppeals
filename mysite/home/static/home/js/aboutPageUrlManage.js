689b9af6e40b4e4f8cf145086a2e3061602665b6
Denis <kanukov.denis@gmail.com>
2022-08-24 17:55:04 +0300

    upd header + add dropdown

--- mysite/home/static/home/js/aboutPageUrlManage.js
+++ mysite/home/static/home/js/aboutPageUrlManage.js
@@ -1,8 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
 	console.log(window.location.hash)
 
-	if (window.location.hash === "#help-tab") {
-		console.log("help")
-		document.getElementById("help-tab").click();
+	switch (window.location.hash) {
+		case "#help-tab": document.getElementById("help-tab").click(); break;
+		case "#docs-tab": document.getElementById("docs-tab").click(); break;
+		case "#requisites-tab": document.getElementById("requisites-tab").click(); break;
+		case "#feedbacks-tab": document.getElementById("feedbacks-tab").click(); break;
+		default: document.getElementById("about-tab").click();
+	}
+
+	window.onhashchange = () => {
+		switch (window.location.hash) {
+			case "#help-tab": document.getElementById("help-tab").click(); break;
+			case "#docs-tab": document.getElementById("docs-tab").click(); break;
+			case "#requisites-tab": document.getElementById("requisites-tab").click(); break;
+			case "#feedbacks-tab": document.getElementById("feedbacks-tab").click(); break;
+			default: document.getElementById("about-tab").click();
+		}
 	}
 });
