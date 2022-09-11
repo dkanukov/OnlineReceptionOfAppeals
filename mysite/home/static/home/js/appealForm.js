77be9a3c03caa5f9abc34d3d1c6b7b9b753a5ca8
Dima <dvdegtyarev@edu.hse.ru>
2022-09-11 14:16:39 +0300

    prod commit

--- mysite/home/static/home/js/appealForm.js
+++ mysite/home/static/home/js/appealForm.js
@@ -65,7 +65,7 @@ document.addEventListener("DOMContentLoaded", () => {
 			option: optionAppeal
 		}
 
-		await fetch("http://127.0.0.1:8000/api/appeal", {
+		await fetch("http://dfvrn.pythonanywhere.com/api/appeal", {
 			method: "POST", headers: {
 				'Accept': 'application/json', 'Content-Type': 'application/json'
 			}, body: JSON.stringify(appealForm)
--- mysite/home/static/home/js/feedback.js
+++ mysite/home/static/home/js/feedback.js
@@ -68,7 +68,7 @@ document.addEventListener("DOMContentLoaded", () => {
 			rating: [...document.getElementsByClassName("rating__star__active")].length
 		}
 
-		await fetch("http://127.0.0.1:8000/api/feedback", {
+		await fetch("http://dfvrn.pythonanywhere.com/api/feedback", {
 			method: "POST", headers: {
 				'Accept': 'application/json', 'Content-Type': 'application/json'
 			}, body: JSON.stringify(feedbackJSON)
