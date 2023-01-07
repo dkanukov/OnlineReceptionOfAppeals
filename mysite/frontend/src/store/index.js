e730b41c61b3e5ff59730531c8203cc132c1cecc
Denis <kanukov.denis@gmail.com>
2023-01-07 17:29:09 +0300

    патч на изменение тикета

--- mysite/frontend/src/store/index.js
+++ mysite/frontend/src/store/index.js
@@ -23,13 +23,18 @@ export default createStore({
 				console.error(`Error during fetch in mount: ${e}`)
 			}
 		},
+		// csrftoken=7ci2ME64hzdWCM3Q0jNDy8efDq9v0twfUsD0Ltfj8vQJXijnluvPPE5MTBSHPFqS
+		// _ym_uid=1662063435230778318; _ym_d=1662063435; _ym_isad=2; csrftoken=7ci2ME64hzdWCM3Q0jNDy8efDq9v0twfUsD0Ltfj8vQJXijnluvPPE5MTBSHPFqS
 		async patchNewTicketStatusById(ctx, element) {
-			console.log(element)
+			const cookie = document.cookie
+			console.log(cookie.substring(cookie.indexOf('csrftoken')))
 			ctx.commit('setNewTicketStatusById', element)
-			await fetch('http://127.0.0.1:8000/api/appeal', {
+			await fetch(`http://127.0.0.1:8000/api/appeal/${element.elementId}`, {
 				method: 'PATCH',
+				headers: {
+					'X-CSRFToken': cookie.substring(cookie.indexOf('csrftoken'))
+				},
 				body: JSON.stringify({
-					'id': element.elementId,
 					'status': element.newStatus
 				})
 			})
