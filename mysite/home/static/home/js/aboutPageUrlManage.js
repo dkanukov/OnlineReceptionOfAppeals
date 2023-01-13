d5eabda7cc41849ef281a58617b11fc9d3465aa2
Denis <kanukov.denis@gmail.com>
2023-01-13 19:14:25 +0300

    форма для доната

--- mysite/home/static/home/js/aboutPageUrlManage.js
+++ mysite/home/static/home/js/aboutPageUrlManage.js
@@ -1,5 +1,25 @@
+const btnSendDonate = document.getElementById('btnSendDonate')
+const inputSendDonate = document.getElementById('inputSendDonate')
 document.addEventListener("DOMContentLoaded", () => {
-	// console.log(window.location.hash)
+	if (inputSendDonate.value === '') {
+		btnSendDonate.disabled = true;
+	}
+
+	inputSendDonate.addEventListener('input', () => {
+		btnSendDonate.disabled = inputSendDonate.value === '';
+	})
+
+	btnSendDonate.addEventListener('click', async () => {
+		console.log(`http//:127.0.0.1:8000/api/pay?value=${inputSendDonate.value.trim()}`)
+		const res = await fetch('http//:127.0.0.1:8000/api/pay?value=' + inputSendDonate.value,
+			{
+				method: 'GET'
+			}
+		)
+		if (res.ok) {
+			console.log('success')
+		}
+	})
 
 	switch (window.location.hash) {
 		case "#help-tab": document.getElementById("help-tab").click(); break;
