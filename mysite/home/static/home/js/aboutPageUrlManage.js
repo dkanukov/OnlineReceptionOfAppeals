7973b5db6086505c59a762f326a15240bfa87701
Denis <kanukov.denis@gmail.com>
2023-01-24 02:58:40 +0300

    правки по страницам: о фонде, главная, футер и хедер

--- mysite/home/static/home/js/aboutPageUrlManage.js
+++ mysite/home/static/home/js/aboutPageUrlManage.js
@@ -18,38 +18,38 @@ document.addEventListener("DOMContentLoaded", () => {
 		window.location.href = res.redirect_url
 	})
 
-	switch (window.location.hash) {
-		case "#help-tab": document.getElementById("help-tab").click(); break;
-		case "#docs-tab": document.getElementById("docs-tab").click(); break;
-		case "#requisites-tab": document.getElementById("requisites-tab").click(); break;
-		case "#feedbacks-tab": document.getElementById("feedbacks-tab").click(); break;
-		default: document.getElementById("about-tab").click();
-	}
+	// switch (window.location.hash) {
+	// 	case "#help-tab": document.getElementById("help-tab").click(); break;
+	// 	case "#docs-tab": document.getElementById("docs-tab").click(); break;
+	// 	case "#requisites-tab": document.getElementById("requisites-tab").click(); break;
+	// 	case "#feedbacks-tab": document.getElementById("feedbacks-tab").click(); break;
+	// 	default: document.getElementById("about-tab").click();
+	// }
 
-	document.getElementById("help-tab").addEventListener('click', () => {
-		location.hash = '#help-tab'
-	})
-	document.getElementById("docs-tab").addEventListener('click', () => {
-		location.hash = '#docs-tab'
-	})
-	document.getElementById("requisites-tab").addEventListener('click', () => {
-		location.hash = '#requisites-tab'
-	})
-	document.getElementById("feedbacks-tab").addEventListener('click', () => {
-		location.hash = '#requisites-tab'
-	})
-	document.getElementById("about-tab").addEventListener('click', () => {
-		location.hash = 'about-tab'
-	})
+	// document.getElementById("help-tab").addEventListener('click', () => {
+	// 	location.hash = '#help-tab'
+	// })
+	// document.getElementById("docs-tab").addEventListener('click', () => {
+	// 	location.hash = '#docs-tab'
+	// })
+	// document.getElementById("requisites-tab").addEventListener('click', () => {
+	// 	location.hash = '#requisites-tab'
+	// })
+	// document.getElementById("feedbacks-tab").addEventListener('click', () => {
+	// 	location.hash = '#requisites-tab'
+	// })
+	// document.getElementById("about-tab").addEventListener('click', () => {
+	// 	location.hash = 'about-tab'
+	// })
 
 
-	window.onhashchange = () => {
-		switch (window.location.hash) {
-			case "#help-tab": document.getElementById("help-tab").click(); break;
-			case "#docs-tab": document.getElementById("docs-tab").click(); break;
-			case "#requisites-tab": document.getElementById("requisites-tab").click(); break;
-			case "#feedbacks-tab": document.getElementById("feedbacks-tab").click(); break;
-			default: document.getElementById("about-tab").click();
-		}
-	}
+	// window.onhashchange = () => {
+	// 	switch (window.location.hash) {
+	// 		case "#help-tab": document.getElementById("help-tab").click(); break;
+	// 		case "#docs-tab": document.getElementById("docs-tab").click(); break;
+	// 		case "#requisites-tab": document.getElementById("requisites-tab").click(); break;
+	// 		case "#feedbacks-tab": document.getElementById("feedbacks-tab").click(); break;
+	// 		default: document.getElementById("about-tab").click();
+	// 	}
+	// }
 });
