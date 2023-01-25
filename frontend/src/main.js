fece9e51a34ef2329dd5ff9aa26679a212351ffb
Dima <dvdegtyarev@edu.hse.ru>
2023-01-26 02:02:51 +0300

    поменял конфиг файл

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import store from './store'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
-import store from '@/store';
 
 loadFonts()
 
 createApp(App)
-	.use(vuetify)
-	.use(store)
-	.mount('#app')
+  .use(store)
+  .use(vuetify)
+  .mount('#app')
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,12 +1,25 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? '/static/dist/' : 'http://127.0.0.1:8080',
     outputDir: '../mysite/static/dist',
-    indexPath: '../../templates/base-vue.html', // relative to outputDir!
 
-    chainWebpack: config => {
-        config.devServer
-            .public('http://127.0.0.1:8080')
-            .hotOnly(true)
-            .headers({"Access-Control-Allow-Origin": "*"})
-            .writeToDisk(filePath => filePath.endsWith('index.html'));
+    // relative to outputDir!
+    indexPath: '../../templates/base-vue.html',
+
+    devServer: {
+            host: '127.0.0.1',
+            port: '8080',
+            headers: {
+            'Access-Control-Allow-Origin': '*',
+        },
+
+            devMiddleware: {
+                      writeToDisk: true,
+                    },
+    },
+
+    pluginOptions: {
+      vuetify: {
+   // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+  }
     }
+}
